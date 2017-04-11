aebfbc7b43060aebe5ef7d52e9be6684273c08c8
Jenna <jenna.galka@gmail.com>
2017-04-11 13:43:21 -0400

    the whole game

---
+++ CodeLab1-2017 copy/Assets/scripts/Week10/AsteroidData.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+
+public class AsteroidData : MonoBehaviour {
+
+	public Vector3 position;
+	public Vector3 scale;
+
+	GameObject[] spheres;
+
+	Vector3[] transforms;
+
+	private const string POS_X = "xpos";
+	private const string POS_Y = "ypos";
+	private const string POS_Z = "zpos";
+
+	public AsteroidData(string fileName){
+		
+		JSONArray jason = UtilScript.ReadJSONFromFile(Application.dataPath, fileName) as JSONArray;
+		Debug.Log (jason);
+
+		for (int i = 0; i < jason.Count; i++) {
+			position = new Vector3(jason[i][POS_X].AsFloat, jason[i][POS_Y].AsFloat, jason[i][POS_Z].AsFloat);
+			GameObject asteroid = Instantiate (Resources.Load ("Prefabs/Sphere", typeof(GameObject))) as GameObject;
+			asteroid.transform.position = position;
+			//sphere[i].transform.position= new Vector3(jason[i][POS_X].AsFloat, jason[i][POS_Y].AsFloat, jason[i][POS_Z].AsFloat);
+			//jason[i] = ToJSON();
+			Debug.Log(position);
+		}
+
+		//GameObject spheres[] = GameObject.FindGameObjectsWithTag("Asteroid");
+
+//		for (int i = 0; i < jason.Count; i++) {
+//
+//			position = new Vector3(jason[i][POS_X].AsFloat, jason[i][POS_Y].AsFloat, jason[i][POS_Z].AsFloat);
+//
+//		}
+	}
+
+	public AsteroidData(Vector3 position, Vector3 scale){
+		
+		this.position = position;
+		this.scale = scale;
+	}
+
+	public JSONNode ToJSON(){
+		
+		JSONNode json = new JSONClass();
+		json[POS_X].AsFloat = position.x;
+		json[POS_Y].AsFloat = position.y;
+		json[POS_Z].AsFloat = position.z;
+		return json;
+
+	}
+
+	public void Save(string fileName, JSONNode json){
+
+		Debug.Log (json);
+		//UtilScript.WriteJSONtoFile(Application.dataPath, fileName, json);
+	}
+}
---
+++ CodeLab1-2017 copy/Assets/scripts/Week10/Loader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+		AsteroidData loadAD = new AsteroidData("Asteroid.txt");
+		print(loadAD.position);
+
+//		for (int i = 0; i < 4; i++) {
+//			GameObject sphere = Instantiate (Resources.Load ("Prefabs/Sphere") as GameObject);
+//			sphere.transform.position = loadAD [i];
+//
+//		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
